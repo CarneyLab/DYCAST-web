58c50aa11b78dc3ce44d997522e38fad9114996a
Vincent <vmeijer@usf.edu>
2017-10-14 16:11:02 -0400

    Initial .NET Core project for dycast-web

---
+++ dycast-web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dycast_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
