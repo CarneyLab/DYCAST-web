01a17bdf33f22652bfb8e417448468b35c4fc77f
Vincent <meijer.vincent@gmail.com>
2017-10-23 15:42:20 -0400

    DEV-407 DEV-408 Initial setup for map that displays points. Project points to correct coordinate system. Use d3js to do the ajax call and re-use the resulting data object.

--- dycast-web/Controllers/MapController.cs
+++ dycast-web/Controllers/MapController.cs
@@ -7,6 +7,8 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using GeoJSON.Net.Feature;
 using GeoJSON.Net.Geometry;
+using DotSpatial.Projections;
+using System.Diagnostics;
 
 namespace dycast_web.Controllers
 {
@@ -20,18 +22,52 @@ namespace dycast_web.Controllers
 
         public IActionResult GetRisk()
         {
+            var projectionFrom = ProjectionInfo.FromEpsgCode(3857);
+            var projectionTo = ProjectionInfo.FromEpsgCode(4326);
+
+            var data = new double[,] {
+                { -5238718.136,-2619612.085,10,1,28,1,0.0484 },
+                { -5238718.136,-2619712.085,10,1,28,1,0.0484 },
+                { -5238718.136,-2619912.085,10,1,30,1,0.5168 },
+                { -5238618.136,-2619312.085,10,1,28,1,0.0484 },
+                { -5238618.136,-2619412.085,10,1,28,1,0.0484 },
+                { -5238618.136,-2619512.085,10,1,28,1,0.0484 },
+                { -5238618.136,-2619612.085,10,1,28,1,0.0484 },
+                { -5238618.136,-2619712.085,10,1,28,1,0.0484 },
+                { -5238618.136,-2619812.085,11,1,36,1,0.0634 },
+                { -5238618.136,-2619912.085,11,1,36,1,0.0634 },
+                { -5238618.136,-2620012.085,11,1,36,1,0.0634 },
+                { -5238518.136,-2619412.085,10,1,28,1,0.0484 },
+                { -5238518.136,-2619512.085,10,1,28,1,0.0484 },
+                { -5238518.136,-2619612.085,10,1,28,1,0.0484 },
+                { -5238518.136,-2619712.085,10,1,28,1,0.0484 },
+                { -5238518.136,-2619812.085,10,1,28,1,0.0484 },
+                { -5238518.136,-2619912.085,10,1,28,1,0.0484 },
+                { -5238418.136,-2619412.085,10,1,28,1,0.0484 },
+            };
+
             var model = new FeatureCollection();
 
-            for (var i = 10; i-- > 0;)
+            for (var i = data.GetLength(0); i-- > 0;)
             {
+
+                var pointsArray = new double[2];
+                pointsArray[0] = data[i, 0];
+                pointsArray[1] = data[i, 1];
+
+                double[] z = { 0 };
+
+                Reproject.ReprojectPoints(pointsArray, z, projectionFrom, projectionTo, 0, 1);
+
                 var point = new Point(
-                    new Position(41.940403 + i, -87.637596 + i));
+                    new Position(pointsArray[1], pointsArray[0]));
+
 
                 var featureProperties = new Dictionary<string, object>
             {
                 { "title", "Point " + i.ToString() },
                 { "description", "This is point number " + i.ToString() },
-                { "pValue", (float)i/100 },
+                { "pValue", data[i,6] },
             };
 
                 var feature = new Feature(point, featureProperties);
@@ -49,6 +85,5 @@ namespace dycast_web.Controllers
 
             return Content(serializedFeature, "application/json");
         }
-
     }
 }
